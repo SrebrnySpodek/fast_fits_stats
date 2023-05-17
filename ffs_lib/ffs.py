18badbd68687a063c77e4432ae41a39a4d0b6478
Marek Górski <mareksgorski@gmail.com>
2023-05-17 23:47:42 +0200

    method improved and coed cleaning

--- ffs_lib/ffs.py
+++ ffs_lib/ffs.py
@@ -1,21 +1,60 @@
-#!/usr/bin/env python3
+
 
 import numpy
 from scipy.signal import convolve2d
 from scipy.signal import find_peaks
 from scipy.ndimage.filters import maximum_filter
 
-import matplotlib.pyplot as plt
+"""
+    FFS (Fast Fits Statistics) library for star detection in an image, and basic statistics. 
+
+    Args:
+        image (numpy.ndarray): The input image.
+        threshold (float, optional): The threshold value for star detection. Defaults to 5.
+        gain (float, optional): Detector gain value. Used for noise estimation. Defaults to 1.
+        rn_noise (float, optional): Detector readout noise. Used for noise estimation. Defaults to 0.
+        method (str, optional): The method used for determining the sigma value. Can be 'rms Poisson','rms','sigma quantile'. Defaults to "sigma quantile".
+        kernel_size (int, optional): The size of the Gaussian kernel. Defaults to 9.
+        fwhm (float, optional): FWHM value. Defaults to 2.
+
+    Attributes:
+        all Args
+        min (float): The minimum value of the ADU.
+        max (float): The maximum value of the ADU.
+        mean (float): The mean value of the ADU.
+        median (float): The median value of the ADU.
+        rms (float): The root mean square value of the ADU.
+        sigma_quantile (float): The sigma value calculated as the quantile 0.5 - 0.159 .
+        noise (float): The noise calculated as the Poisson noise accounting gain and readout noise.
+
+    Methods:
+        find_stars(): Finds the stars in the image with specified noise calulation method.
+    
+        Returns:
+            coo (numpy.ndarray): An sorted array of coordinates representing the positions of stars.
+            adu (numpy.ndarray): An sorted array of ADU values corresponding to the detected stars.
+
+    Example usage:
+        stats = FFS(data,threshold=5,kernel_size=9,fwhm=6)
+        sigma = stats.sigma_quantile
+        p_noise = stats.noise
+        coo,adu = stats.find_stars()
+
+"""
+
+
 
 class FFS:
 
-    def __init__(self,image,threshold=10.,gain=1.,rn_noise=0.,method="sigma quantile",kernel_type="3x3 Gauss"):
+    def __init__(self,image,threshold=5.,gain=1.,rn_noise=0.,method="sigma quantile",kernel_size=9,fwhm=2):
         self.image = numpy.transpose(image)
-        self.threshold = threshold
-        self.gain = gain
-        self.rn_noise = rn_noise
+        self.threshold = float(threshold)
+        self.gain = float(gain)
+        self.rn_noise = float(rn_noise)
         self.method = method
-        self.kernel_type = kernel_type
+        self.fwhm = float(fwhm)
+        self.kernel_size = int(kernel_size)
+        self.kernel_sigma = float(fwhm)/2.355
 
         self.min = numpy.min(image)
         self.max = numpy.max(image)
@@ -25,23 +64,14 @@ class FFS:
         self.sigma_quantile = numpy.median(image)-numpy.quantile(image, 0.159)
         self.noise = (self.median/self.gain+self.rn_noise)**0.5             
 
+    def gauss_kernel(self,size,sigma):
+        kernel = numpy.fromfunction(lambda x, y: (1/(2*numpy.pi*sigma**2)) * numpy.exp(-((x-(size-1)/2)**2+(y-(size-1)/2)**2)/(2*sigma**2)),(size,size))
+        return kernel / numpy.sum(kernel)
+
+
     def find_stars(self):
 
-        if self.kernel_type == "3x3 Gauss":
-          self.kernel = [
-                        [0.077847, 0.123317, 0.077847],
-                        [0.123317, 0.195346, 0.123317],
-                        [0.077847, 0.123317, 0.077847]
-                        ]
-        elif self.kernel_type == "5x5 Gauss":
-          self.kernel = [
-                        [0.003765, 0.015019, 0.023792, 0.015019, 0.003765],
-                        [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
-                        [0.023792, 0.094907, 0.150342, 0.094907, 0.023792],
-                        [0.015019, 0.059912, 0.094907, 0.059912, 0.015019],
-                        [0.003765, 0.015019, 0.023792, 0.015019, 0.003765]
-                        ]
-        else: raise ValueError(f"Invalid kernel type {self.kernel_type}")   
+        self.kernel = self.gauss_kernel(self.kernel_size,self.kernel_sigma)
 
         if self.method == "rms Poisson":
           self.sigma = self.noise
@@ -50,6 +80,7 @@ class FFS:
         elif self.method == "sigma quantile":
           self.sigma = self.sigma_quantile
         else: raise ValueError(f"Invalid method type {self.method}") 
+
 
         maska1 = self.image > self.median + self.threshold * self.sigma
         data2 = convolve2d(self.image, self.kernel, mode='same')
@@ -65,18 +96,5 @@ class FFS:
         self.coo = sorted_coo
         self.adu = sorted_val
 
-    def check_distribution(self):
-      xl,yl = zip(*self.coo)
-      distances = []
-      for i,tmp in self.coo:
-        if i < 100:
-          x = self.coo[i][0]
-          y = self.coo[i][1]
-          dx = x - xl
-          dy = y - yl
-          distances = numpy.append(distances, dx)
-          distances = numpy.append(distances, dy)
+        return self.coo, self.adu
 
- 
-      plt.hist(distances, bins=50)
-      plt.show()
--- find_stars.py
+++ find_stars.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
 
-import sys,os
+import sys,os, time
 from astropy.io import fits
 from ffs_lib.ffs import FFS 
+
+
+"""A script for star detection for a FITS files, and basic statistics calculation.  
+
+Usage:
+    find_stars.py <fits_path> [options]
+
+Arguments:
+    <fits_path>      Required. The path to the FITS file.
+
+Options:
+    threshold=<value>       The threshold value for star detection. Defaults to 5. Really, should't be smaller than 3!!!
+    fwhm=<value>            FWHM value. Defaults to 2.
+    kernel_size=<size>      The size of the Gaussian kernel. Defaults to 9.
+    method=<method>         The method used for determining the sigma value. Can be 'rms Poisson','rms','sigma quantile'. Defaults to "sigma quantile".
+    gain=<value>            Detector gain value. Used for noise estimation. Defaults to 1.
+    rn_noise=<value>        Detector readout noise. Used for noise estimation. Defaults to 0.
+
+
+Returns: Basic statistics and .ffs file (in the directory of <fits_path>) with stars coordinates and coresponding ADU
+
+Example usage: 
+    ./find_stars.py ./DATA/zb08c_0014_43387.fits threshold=2 kernel_size=15 fwhm=10
+
+
+"""  
 
 
 def main():
@@ -15,31 +41,43 @@ def main():
             kwargs[key] = value
         else:
             path = arg   
+    ok = False
+    try:
+        hdu=fits.open(path)
+        data = hdu[0].data
+        ok = True
+    except FileNotFoundError: 
+        print(f"File {path} not found")
+    except OSError: 
+        print(f"File {path} is not a valid FITS file")
+        
+    if ok:
+        stats = FFS(data,**kwargs)
+        coo,adu = stats.find_stars()
     
-    hdu=fits.open(path)
-    hdr = data = hdu[0].header 
-    data = hdu[0].data
+        txt = f"stars no.: {len(coo)}\n"
+        txt = txt + f"min ADU: {stats.min}\n"
+        txt = txt + f"max ADU: {stats.max}\n"
+        txt = txt + f"mean: {stats.mean}\n"
+        txt = txt + f"median: {stats.median}\n"
+        txt = txt + f"rms: {stats.rms}\n"
+        txt = txt + f"q(0.5)-q(0.159): {stats.sigma_quantile}\n"
+        txt = txt + f"Poisson noise: {stats.noise}\n"
+        print(txt)
 
-    stats = FFS(data,threshold=5)
-    stats.find_stars()
-    stats.check_distribution()
+        txt = f"# stars no.: {len(coo)}\n"
+        txt = txt + f"# min ADU: {stats.min}\n"
+        txt = txt + f"# max ADU: {stats.max}\n"
+        txt = txt + f"# mean: {stats.mean}\n"
+        txt = txt + f"# median: {stats.median}\n"
+        txt = txt + f"# rms: {stats.rms}\n"
+        txt = txt + f"# q(0.5)-q(0.159): {stats.sigma_quantile}\n"
+        txt = txt + f"# Poisson noise: {stats.noise}\n"
+        txt = txt + f"# x_coo    y_coo    adu_xy\n\n"
 
-
-    coo = stats.coo 
-
-
-    print("stars no.: ",len(coo))
-
-    txt="\n \n"
-    for cooxy in coo:
-      txt = txt + "1 "+str(int(cooxy[0])+1)+" "+str(int(cooxy[1])+1)+"\n"
-    with open("result.coo", mode='w') as plik: plik.write(txt)
-
-
-    hdu2 = fits.PrimaryHDU(data)
-    hdu2.header = hdr
-    hdu2.writeto('result.fits',overwrite=True)
-
+        for i,tmp in enumerate(coo):
+          txt = txt +str(int(coo[i][0])).ljust(6)+" "+str(int(coo[i][1])).ljust(6) +" "+ str(adu[i])+"\n"
+        with open(path.replace(".fits", ".ffs"), mode='w') as plik: plik.write(txt)
 
 if __name__ == "__main__":
     main()
